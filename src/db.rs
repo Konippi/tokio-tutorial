a142877e7112d7b7643c93e8e7da578da0dea53b
Konippi <konippi-114@outlook.jp>
2024-12-21 22:35:34 +0900

    chore: separate db into a new file

--- src/db.rs
+++ src/db.rs
@@ -5,4 +5,25 @@ use std::{
 
 use bytes::Bytes;
 
-pub type Db = Arc<Mutex<HashMap<String, Bytes>>>;
+#[derive(Debug, Clone)]
+pub struct Db {
+    pub data: Arc<Mutex<HashMap<String, Bytes>>>,
+}
+
+impl Db {
+    pub fn new() -> Self {
+        Self {
+            data: Arc::new(Mutex::new(HashMap::new())),
+        }
+    }
+
+    pub fn insert(&self, key: String, value: Bytes) {
+        let mut db = self.data.lock().unwrap();
+        db.insert(key, value);
+    }
+
+    pub fn get(&self, key: &str) -> Option<Bytes> {
+        let db = self.data.lock().unwrap();
+        db.get(key).cloned()
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,5 @@
 mod db;
 
-use std::{
-    collections::HashMap,
-    sync::{Arc, Mutex},
-};
-
 use db::Db;
 use mini_redis::{
     Command::{self, Get, Set},
@@ -20,13 +15,14 @@ pub async fn main() {
 
     println!("Listening on port 6379...");
 
-    let db = Arc::new(Mutex::new(HashMap::new()));
+    let db = Db::new();
 
     loop {
         let (socket, _) = listener.accept().await.unwrap();
-        let db = db.clone();
 
         println!("Accepted connection from {:?}", socket.peer_addr());
+
+        let db = db.clone();
 
         tokio::spawn(async move {
             process(socket, db).await;
@@ -40,12 +36,10 @@ async fn process(socket: TcpStream, db: Db) {
     while let Some(frame) = connection.read_frame().await.unwrap() {
         let response = match Command::from_frame(frame).unwrap() {
             Set(cmd) => {
-                let mut db = db.lock().unwrap();
                 db.insert(cmd.key().to_string(), cmd.value().clone());
                 Frame::Simple("OK".to_string())
             }
             Get(cmd) => {
-                let db = db.lock().unwrap();
                 if let Some(value) = db.get(cmd.key()) {
                     Frame::Bulk(value.clone())
                 } else {
