2e3343602edd3c76862458475ce4eb599df653bd
Konippi <konippi-114@outlook.jp>
2024-12-21 11:58:59 +0900

    chore: add redis server

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,30 @@
-use std::str::from_utf8;
-
-use mini_redis::{client, Result};
+use mini_redis::{Connection, Frame};
+use tokio::net::{TcpListener, TcpStream};
 
 #[tokio::main]
-pub async fn main() -> Result<()> {
-    let mut client = client::connect("127.0.0.1:6379").await?;
+pub async fn main() {
+    let listener = TcpListener::bind("127.0.0.1:6379")
+        .await
+        .expect("failed to bind to address");
 
-    client.set("user", "password".into()).await?;
-
-    let key = "user";
-    let result = client.get(key).await?;
-    match result {
-        Some(value) => {
-            println!("Got: {}", from_utf8(&value)?);
-        }
-        None => {
-            println!("Not found: {}", key);
-        }
+    loop {
+        let (scoket, _) = listener.accept().await.expect("failed to accept");
+        tokio::spawn(async move {
+            process(scoket).await;
+        });
     }
+}
 
-    Ok(())
+async fn process(socket: TcpStream) {
+    let mut connection = Connection::new(socket);
+
+    if let Some(frame) = connection.read_frame().await.expect("failed to read frame") {
+        println!("GOT: {:?}", frame);
+
+        let response = Frame::Error("unimplemented".to_string());
+        connection
+            .write_frame(&response)
+            .await
+            .expect("failed to write frame");
+    }
 }
